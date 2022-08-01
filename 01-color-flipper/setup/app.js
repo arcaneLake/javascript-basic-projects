85fceed44e60fe334d12812ade71ce05f27bb722
arcaneLake <geisnerm@gmail.com>
2022-08-01 14:39:20 +0900

    Rewrite simple part

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -3,12 +3,17 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 // make btn equal to code to get btn ID from dom
 const btn = document.getElementById("btn");
-console.log(btn);
 
 // make color equal to code to get .class from doc
 const color = document.querySelector(".color");
-console.log(color);
 
-btn.addEventListener("click",
-    const randomNumber = getRandomNum();
-    );
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
